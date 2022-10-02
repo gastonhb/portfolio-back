72182393a8e0860b08d8fef3b3396d3a3f1ca103
Gaston <gaston.herrerabaron@outlook.com>
2022-10-01 21:09:41 -0300

    Update EducationResponseDTO.java

--- src/main/java/com/portfolio/back/dto/EducationResponseDTO.java
+++ src/main/java/com/portfolio/back/dto/EducationResponseDTO.java
@@ -1,5 +1,6 @@
 package com.portfolio.back.dto;
 
+import java.util.Date;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +10,15 @@ public class EducationResponseDTO {
     private UUID id;
     private String title;
     private String institute;
-    private Integer startDate;
-    private Integer endDate;
+    private Date startDate;
+    private Date endDate;
     private String urlImage;
     private UUID personId;
 
     public EducationResponseDTO() {
     }
 
-    public EducationResponseDTO(UUID id, String title, String institute, Integer startDate, Integer endDate, String urlImage, UUID personId) {
+    public EducationResponseDTO(UUID id, String title, String institute, Date startDate, Date endDate, String urlImage, UUID personId) {
         this.id = id;
         this.title = title;
         this.institute = institute;
@@ -26,7 +27,5 @@ public class EducationResponseDTO {
         this.urlImage = urlImage;
         this.personId = personId;
     }
-    
-    
     
 }
